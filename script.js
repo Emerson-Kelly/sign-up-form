39f0ff5e101a790a2d86a9c384ffc683f72496ef
Emerson Kelly <123923271+Emerson-Kelly@users.noreply.github.com>
2024-03-13 19:32:30 -0400

    Add Script to Validate or Invalidate Password Confirmation

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 document.getElementById('account').addEventListener('submit', function (event) {
-    var form = event.target;
-    var inputs = form.querySelectorAll('input');
+    let form = event.target;
+    let inputs = form.querySelectorAll('input');
+    let password = form.querySelector('#password');
+    let confirmPassword = form.querySelector('#confirm-password');
+    let errorMessage = form.querySelector('#confirm-password + .error-message');
+
+    if (password.value !== confirmPassword.value) {
+        confirmPassword.style.backgroundColor = "#ffdddd";
+        confirmPassword.setCustomValidity('Passwords do not match.');
+        confirmPassword.classList.add('invalid');
+        errorMessage.style.display = 'block';
+    } else {
+        confirmPassword.style.backgroundColor = "#ddffdd";
+        confirmPassword.setCustomValidity('');
+        confirmPassword.classList.remove('invalid');
+        errorMessage.style.display = 'none';
+    }
+
     inputs.forEach(function (input) {
         if (!input.checkValidity()) {
             input.classList.add('invalid');
@@ -10,7 +26,6 @@ document.getElementById('account').addEventListener('submit', function (event) {
         }
     });
 
-    // Prevent form submission if there are invalid fields
     if (!form.checkValidity()) {
         event.preventDefault();
     }
