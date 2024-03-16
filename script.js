a920156aaa86ded0679a9a593510e9eddc3328b3
Emerson Kelly <123923271+Emerson-Kelly@users.noreply.github.com>
2024-03-15 20:21:56 -0400

    Fix User Validation Bug

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 document.getElementById('account').addEventListener('submit', function (event) {
-    let form = event.target;
-    let inputs = form.querySelectorAll('input');
-    let password = form.querySelector('#password');
-    let confirmPassword = form.querySelector('#confirm-password');
-    let errorMessage = form.querySelector('#confirm-password + .error-message');
+    var form = event.target;
+    var inputs = form.querySelectorAll('input');
+    var password = form.querySelector('#password');
+    var confirmPassword = form.querySelector('#confirm-password');
+    var errorMessage = form.querySelector('#confirm-password + .error-message');
 
     if (password.value !== confirmPassword.value) {
-        confirmPassword.style.backgroundColor = "#ffdddd";
         confirmPassword.setCustomValidity('Passwords do not match.');
         confirmPassword.classList.add('invalid');
         errorMessage.style.display = 'block';
     } else {
-        confirmPassword.style.backgroundColor = "#ddffdd";
         confirmPassword.setCustomValidity('');
         confirmPassword.classList.remove('invalid');
         errorMessage.style.display = 'none';
